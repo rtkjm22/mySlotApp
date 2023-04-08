32aea44eab19670498b54cd3a91df6828eaa9510
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2023-04-09 01:19:27 +0900

    ADD:リール目の追加

---
+++ src/components/Molecules/Reel.tsx
@@ -0,0 +1,65 @@
+const Reel = ({ position, active, reelArr, resultArr }) => {
+  return (
+    <div className="w-full h-[600px] bg-white transition-all overflow-hidden">
+      {/* リール目 */}
+      <div
+        className={`transition-all duration-300 ${
+          active ? 'visible opacity-100' : `opacity-0 h-0`
+        }`}
+      >
+        <ul className={`text-blue-500 animate-slide-bottom-${position}`}>
+          {reelArr.map((item, index) => (
+            <li
+              key={`${item}-${index}`}
+              className={`w-full h-[200px] text-center border ${
+                index % 2 === 0 ? 'bg-blue-50' : ''
+              }`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+        <ul
+          className={`text-red-500 opacity-0 animate-slide-bottom-${position}`}
+        >
+          {reelArr.map((item, index) => (
+            <li
+              key={`${item}2-${index}`}
+              className={`w-full h-[200px] text-center border ${
+                index % 2 === 0 ? 'bg-red-50' : ''
+              }`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* 結果 */}
+      <div
+        className={`transition-all duration-300 ${
+          active ? 'invisible' : 'visible'
+        }`}
+      >
+        <ul
+          className={`text-red-500 ${
+            active ? '' : 'animate-slide-bottom-delay'
+          }`}
+        >
+          {resultArr.map((item, index) => (
+            <li
+              key={`${item}2-${index}`}
+              className={`w-full h-[200px] text-center border bg-yellow-300 ${
+                index % 2 === 0 ? 'bg-red-50' : ''
+              }`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Reel
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,49 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        'slide-bottom-left': 'slide-bottom 2s linear infinite',
+        'slide-bottom-middle': 'slide-bottom 2s linear infinite',
+        'slide-bottom-right': 'slide-bottom 1.8s linear infinite',
+        'slide-bottom-delay': 'slide-bottom-delay 1.5s ease-out',
+        'hidden-item': 'hidden-item .3s ease-out'
       },
-    },
+      keyframes: {
+        'slide-bottom': {
+          from: {
+            opacity: '1',
+            transform: 'translateY(-6000px)'
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        'slide-bottom-delay': {
+          from: {
+            opacity: '1',
+            transform: 'translateY(-900px)'
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)'
+          }
+        },
+        'hidden-item': {
+          from: {
+            opacity: '1'
+          },
+          to: {
+            opacity: '0',
+            display: 'none'
+          }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
